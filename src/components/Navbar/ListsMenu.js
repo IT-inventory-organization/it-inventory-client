1b4492f0c268a36715bdded09987c43c0b660e4a
Ahmad <41444348+ahmad17-fe@users.noreply.github.com>
2021-09-29 14:27:19 +0700

    create list menu

---
+++ src/components/Navbar/ListsMenu.js
@@ -0,0 +1,24 @@
+const ListsMenu = [
+  {
+    name: "Dahsboard",
+    path: "/",
+    icon: "",
+  },
+  {
+    name: "PLB",
+    path: "/plb",
+    icon: "",
+  },
+  {
+    name: "PPFTZ",
+    path: "/ppftz",
+    icon: "",
+  },
+  {
+    name: "Setup",
+    path: "/setup",
+    icon: "",
+  },
+];
+
+export default ListsMenu;
