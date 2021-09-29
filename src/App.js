2ccc88c599e1c3fba09028817461c1dece90f2e3
Ahmad <41444348+ahmad17-fe@users.noreply.github.com>
2021-09-29 14:28:41 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
 import Dashboard from "./views/Dashboard.js";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/">
           <Dashboard />
