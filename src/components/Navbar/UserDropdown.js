882c5e8cd9f711509351bb9746c1f9f5ad0e9a04
Ahmad <41444348+ahmad17-fe@users.noreply.github.com>
2021-09-29 14:27:06 +0700

    create dropdown for button user

---
+++ src/components/Navbar/UserDropdown.js
@@ -0,0 +1,67 @@
+import {
+  Avatar,
+  ClickAwayListener,
+  Divider,
+  experimentalStyled,
+  Grow,
+  Menu,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useState, useRef } from "react";
+
+function UserDropdown() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <DropdownWrap
+        onClick={(e) => handleClick(e)}
+        alignItems="center"
+        spacing={1}
+        direction="row"
+      >
+        <Avatar alt="User" src="" />
+        <Typography style={{ fontWeight: "700" }}>Administrator</Typography>
+        <img
+          style={{ stroke: "red" }}
+          src={require("../../assets/icons/ic_chevron.svg").default}
+          alt=""
+        />
+      </DropdownWrap>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Change Password</MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+}
+
+const DropdownWrap = experimentalStyled(Stack)({
+  background: "#fff",
+  userSelect: "none",
+  cursor: "pointer",
+});
+
+export default React.memo(UserDropdown);
