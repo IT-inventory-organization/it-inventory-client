342b820bccc3bad623b92ff83003918fe6f7231a
Ahmad <ahmads1704@gmail.com>
2021-09-29 23:24:51 +0700

    using ReactComponent for svg

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,9 +1,12 @@
 import { Container, Grid } from "@mui/material";
-import "../styles/dashboard/index.scss";
 import DashboardChart from "../components/dashboard/DashboardChart";
 import DisplayCount from "../components/dashboard/DisplayCount";
 import RecentActivities from "../components/dashboard/RecentActivities";
 import MainLayout from "../layout/MainLayout";
+import {ReactComponent as IcProductIn} from '../assets/icons/ic_productin.svg'
+import {ReactComponent as IcProductOut} from '../assets/icons/ic_productout.svg'
+import {ReactComponent as IcStockOpName} from '../assets/icons/ic_stock_opname.svg'
+import {ReactComponent as IcDataAdjutment} from '../assets/icons/ic_data_adjutment.svg'
 
 function Dashboard() {
   return (
@@ -16,7 +19,7 @@ function Dashboard() {
             count={1300}
             ContainerColor="#3CB774"
             BoxIconColor="#5ECE91"
-            icon={require("../assets/icons/ic_productin.svg").default}
+            icon={<IcProductIn/>}
           />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
@@ -25,7 +28,7 @@ function Dashboard() {
             count={990}
             ContainerColor="#DE3D33"
             BoxIconColor="#ED554B"
-            icon={require("../assets/icons/ic_productout.svg").default}
+            icon={<IcProductOut />}
           />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
@@ -34,7 +37,7 @@ function Dashboard() {
             count={1500}
             ContainerColor="#F9B800"
             BoxIconColor="#FFCB38"
-            icon={require("../assets/icons/ic_stock_opname.svg").default}
+            icon={<IcStockOpName />}
           />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
@@ -43,7 +46,7 @@ function Dashboard() {
             count={1100}
             ContainerColor="#5682FF"
             BoxIconColor="#82A2FF"
-            icon={require("../assets/icons/ic_data_adjutment.svg").default}
+            icon={<IcDataAdjutment />}
           />
         </Grid>
       </Grid>
