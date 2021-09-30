844594a6a6a07b82c1863408c0903ce214486ded
Ahmad <ahmads1704@gmail.com>
2021-09-30 22:27:43 +0700

    menambah fungsi pencarian

--- src/components/EnchancedToolbar.js
+++ src/components/EnchancedToolbar.js
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Grid,
-  Stack,
-  OutlinedInput,
-  InputAdornment,
-} from "@mui/material";
+import { Button, Grid, Stack, OutlinedInput } from "@mui/material";
 import styles from "../styles/dashboard/index.module.scss";
 import classnames from "classnames";
 // import { ReactComponent as IcView } from "../assets/icons/ic_view.svg";
@@ -13,16 +7,16 @@ import classnames from "classnames";
 // import { ReactComponent as IcView } from "../assets/icons/ic_view.svg";
 // import { ReactComponent as IcView } from "../assets/icons/ic_view.svg";
 import { ReactComponent as IcSearch } from "../assets/icons/ic_search.svg";
-import StyledTextField from "./StyledTextField";
 
 function EnchancedToolbar(props) {
-  const { handleOpenModal } = props;
+  const { handleOpenModal, handleChangeSearch, ...other } = props;
   return (
     <Grid className={styles.enchanced_toolbar} container>
       <Grid item lg={6} md={6} sm={6} xs={12}>
         <OutlinedInput
           className={styles.enchanced_toolbar_search}
           startAdornment={<IcSearch />}
+          onChange={(e) => handleChangeSearch(e)}
           placeholder="Cari"
         />
       </Grid>
@@ -44,4 +38,4 @@ function EnchancedToolbar(props) {
   );
 }
 
-export default EnchancedToolbar;
+export default React.memo(EnchancedToolbar);
