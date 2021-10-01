85c291286ecd1da1fc546d28d897b09394f06798
Ahmad <ahmads1704@gmail.com>
2021-10-01 16:57:18 +0700

    Create ModalForm.js
    membuat parent modal agar bisa digunakan kembali

---
+++ src/components/ModalForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Dialog, Stack, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { ReactComponent as IcCancelX } from "../assets/icons/ic_cancelx.svg";
+import styles from "../styles/dashboard/index.module.scss";
+
+function ModalCreate(props) {
+  const { open, close, title, children } = props;
+  return (
+    <StyledDialog fullWidth open={open} onClose={() => close()}>
+      <Stack justifyContent="space-between" direction="row" alignItems="center">
+        <Typography variant="h6">{title}</Typography>
+        <IcCancelX
+          onClick={() => close()}
+          className={styles.modal_create_btn_close}
+        />
+      </Stack>
+      <div style={{ marginTop: "20px", padding: "30px 0" }}>{children}</div>
+    </StyledDialog>
+  );
+}
+
+const StyledDialog = styled(Dialog)`
+  .MuiBackdrop-root {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  .MuiPaper-root {
+    padding: 20px 30px;
+    border-radius: 20px;
+    box-shadow: none;
+    /* top: -10%; */
+  }
+`;
+
+export default ModalCreate;
