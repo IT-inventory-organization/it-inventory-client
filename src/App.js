80539e3b83b684c1930a70c2667c5ce8832b2c9f
Ahmad <ahmads1704@gmail.com>
2021-10-01 16:56:24 +0700

    menambahkan route baru
    route plb/add untuk component createnewplb

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.js";
+import { Navbar } from "./components/Navbar.js";
+import ScrollToTop from "./components/ScrollToTop.js";
 import Dashboard from "./views/Dashboard.js";
-import PLB from "./views/PLB.js";
+import PLB from "./views/PLB";
+import CreateNewPLB from "./views/PLB/CreateNewPLB.js";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Switch>
         <Route exact path="/">
           <Dashboard />
         </Route>
-        <Route path="/plb">
-          <PLB />
-        </Route>
+        <Route
+          path="/plb"
+          render={({ match: { url } }) => (
+            <>
+              <Route path={`${url}`} component={PLB} exact />
+              <Route path={`${url}/add`} component={CreateNewPLB} />
+            </>
+          )}
+        />
       </Switch>
     </Router>
   );
