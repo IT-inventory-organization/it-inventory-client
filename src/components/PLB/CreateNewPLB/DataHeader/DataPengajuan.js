0d350c0d37e0e4c2ca708a0ca8ebc69ec80579f9
Ahmad <ahmads1704@gmail.com>
2021-10-03 08:43:48 +0700

    update layout  form

--- src/components/PLB/CreateNewPLB/DataHeader/DataPengajuan.js
+++ src/components/PLB/CreateNewPLB/DataHeader/DataPengajuan.js
@@ -1,12 +1,12 @@
 import { Grid, Stack } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import StyledTextField from "../../../StyledTextField";
 import StyledAutoCompletedField from "../../../StyledAutoCompletedField";
 
 function DataPengajuan() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={12} lg={6}>
+    <Grid container spacing={2} justifyContent="center">
+      <Grid item xs={12} md={6} lg={6}>
         <Stack gap={2}>
           <StyledAutoCompletedField label="Kantor Pabean Asal" />
           <StyledTextField label="Kantor Pabean Asal" />
@@ -14,7 +14,7 @@ function DataPengajuan() {
         </Stack>
       </Grid>
 
-      <Grid item xs={12} md={12} lg={6}>
+      <Grid item xs={12} md={6} lg={6}>
         <Stack gap={2}>
           <StyledTextField label="Kantor Pabean Asal" />
           <StyledTextField label="Kantor Pabean Asal" />
